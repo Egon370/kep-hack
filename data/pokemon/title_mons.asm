23da560fcc4f315a0417205ce9dc936408b217fa
Egon370 <97806632+Egon370@users.noreply.github.com>
2024-03-01 10:10:34 -0800

    Update title_mons.asm

--- data/pokemon/title_mons.asm
+++ data/pokemon/title_mons.asm
@@ -1,18 +1,19 @@
 TitleMons:
 ; mons on the title screen are randomly chosen from here
-	db TOTARTLE     ; New Starters
-	db GOROCHU
-	db SYLVEON
-	db GAVILLAIN       ; RG Betas
-	db SHARPOON
-	db GAWARHED
-	db BELLIGNAN	; GS Betas
-	db LUXWAN
-	db PORYGON2     ; Post-Gen 1 Evos
-	db MAGNEZONE
-	db ANNIHILAPE
-	db SCREAM_TAIL  ; LGPE and SV
-	db WUGTRIO
-	db MELTAN
-	db EXEGGUTOR_A  ; Regional Variants
-	db TAUROS_PB
+	db BEEDRILL     ; The Bugs
+	db PARASECT
+	db KINGLER
+	db BUTTERFREE
+	db RAPIDASH_G       ; The Fairies
+	db MR_MIME
+	db CLEFABLE
+	db WIGGLYTUFF
+	db GYARADOS	; Sky Attack users
+	db AERODACTYL
+	db GOROTORA     ; Mons w/ new signature moves
+	db MAROWAK_A
+	db PIDGEOT
+	db SANDSLASH_A  ; Personal favorites
+	db IGUANARCH
+	db PENDRAKEN
+	
